47356e1e83745fd4328d66298ef50582943f89fd
CodeJCSON <2334497007@qq.com>
2017-08-18 13:42:45 +0800

    pod添加WMPageController第三方

--- LBNewsComing/LBNewsComing/Classes/controller/main/LBNCNavController.m
+++ LBNewsComing/LBNewsComing/Classes/controller/main/LBNCNavController.m
@@ -13,25 +13,17 @@
 @end
 
 @implementation LBNCNavController
-
++(void)initialize{
+    // 导航背景
+    [[UINavigationBar appearance]setBackgroundImage:[UIImage imageNamed:@"backgroundImage"] forBarMetrics:UIBarMetricsDefault];
+    // 导航栏title字体大小
+    [[UINavigationBar appearance] setTitleTextAttributes:@{NSFontAttributeName:[UIFont boldSystemFontOfSize:20], NSForegroundColorAttributeName:[UIColor whiteColor]}];
+    // 状态栏颜色
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent animated:YES];
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
---
+++ notes.h
@@ -0,0 +1,8 @@
+
+### 笔记记实
+
+1.
+初始化项目，创建标签栏。
+2.
+为导航栏设置背景，为导航栏标题设置颜色、字体，为状态栏设置颜色。
+View controller-based status bar appearance 在info中设置NO；在导航控制器中+(void)initialize{}方法中设置状态栏颜色。
